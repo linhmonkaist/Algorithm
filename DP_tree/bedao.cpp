2a97d5a65d10f9bc8393a6272ea1094577748151
linhmonkaist <nguyenlinhmon@gmail.com>
2023-05-18 14:51:53 +0900

    change folder

--- DP_tree/bedao.cpp
+++ DP_tree/bedao.cpp
@@ -2,24 +2,26 @@
 using namespace std;
 int n;
 vector<vector<int>> tree; 
-vector<int> c; 
-vector<set<int>> p; 
+vector<int> c, tag, rmv; 
 void dfs(int u, int par){
     if (tree[u].size() == 1 && u != 1){
-        p[u].insert(c[u]);
+        tag[u]= c[u]; 
         return; 
     }
     for (int &chld: tree[u]){
         if (chld == par) continue;
         dfs(chld, u); 
-        if (p[chld].size() > 1){
-            cout << "NO";
-            p[]
-            return; 
+        if (c[u] != c[chld] && c[u] != -1 && c[chld] != -1){
+            rmv.push_back(u); 
+            c[u]= -1; 
         }
     } 
 }
 int main(){
+    if (fopen("test.inp", "r")){
+        freopen("test.inp", "r", stdin);
+        freopen("test.out", "w", stdout); 
+    }
     cin >> n; 
     int u, v; 
     tree.resize(n+1);
@@ -32,6 +34,16 @@ int main(){
     for (int i= 1; i< n+1; i++){
         cin >> c[i];
     }
-    p.resize(n+1); 
+    tag.assign(n+1, -1); 
     dfs(1, 0); 
+    if (rmv.size() > 1) cout << "NO";
+    else if (rmv.size() == 1) cout << "YES" << '\n' << rmv[0];
+    else {
+        cout << "YES" << '\n';
+        for (int i= 1; i< n+1; i++){
+            cout << i << ' '; 
+        }
+    }
+    return 0; 
+    
 }
